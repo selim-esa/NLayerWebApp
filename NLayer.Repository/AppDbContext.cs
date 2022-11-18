d81a05fb15c263bac9cfab5f1b2ee862d663f406
Selim <Selim@DESKTOP-5H0MQ1Q>
2022-11-18 20:58:41 +0300

    Add project files.

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options:base (Options))
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+        
+    
+    } 
+}
---
+++ NLayer.WEB/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace NLayer.WEB.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
